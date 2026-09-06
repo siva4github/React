f5e5da5452e90e46608ece96c84e75d0f1452b84
Siva Alamalakala <siva.a@mpinfotech.com>
2026-09-06 15:39:37 -0400

    Storing components in files

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,35 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
 import {CORE_CONCEPTS} from "./data.js";
+import Header from "./Componets/Header.jsx";
+import CoreConcept from "./Componets/CoreConcepts.jsx";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(reactDescriptions.length - 1)]} React
-        concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept({image, title, description}) {}
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt=""/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
