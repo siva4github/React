2ebad54158a0bf85a6af8d8e10c1b9e81bf498fa
Siva Alamalakala <leo.shiv@gmail.com>
2026-09-15 23:24:39 -0400

    multiple jsx slots

--- react-essentials/src/Componets/Examples.jsx
+++ react-essentials/src/Componets/Examples.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import TabButton from "./TabButton.jsx";
 import { EXAMPLES } from "../data.js";
 import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
 
 export default function Examples() {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -27,12 +28,16 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <menu>
-                <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-                <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-                <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-            </menu>
+            <Tabs buttons={
+                <>
+                    <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+                    <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                    <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+                    <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+                </>
+            }>
+                {tabContent}
+            </Tabs>
             {/* {!selectedTopic ? <p>Click on a tab to see an example of that concept.</p> : (
             <div id="tab-content">
               <h3>{EXAMPLES[selectedTopic]?.title}</h3>
@@ -41,7 +46,7 @@ export default function Examples() {
                 {EXAMPLES[selectedTopic]?.code}
               </code></pre>
             </div>)} */}
-            {tabContent}
+
         </Section>
     );
 }
