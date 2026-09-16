92ee3a8ea7acde0d064ddba1a2eb91b2a838ca49
Siva Alamalakala <leo.shiv@gmail.com>
2026-09-15 22:54:26 -0400

    split the components by feature & state

---
+++ react-essentials/src/Componets/CoreConcept.jsx
@@ -0,0 +1,10 @@
+// function CoreConcept({image, title, description}) {}
+export default function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt="" />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
--- react-essentials/src/Componets/CoreConcepts.jsx
+++ react-essentials/src/Componets/CoreConcepts.jsx
@@ -1,10 +1,17 @@
-// function CoreConcept({image, title, description}) {}
-export default function CoreConcept(props) {
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../data.js";
+
+export default function CoreConcepts() {
     return (
-        <li>
-            <img src={props.image} alt="" />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </li>
+        <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+                {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+                {/* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+            </ul>
+        </section>
     );
 }
